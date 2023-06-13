a4b56724d30baefcc6d10533dafe2416e6f292a2
Mateus Jesus <mateusgianetti1996@gmail.com>
2023-06-12 22:42:21 -0300

    chore(icons): fixed index file

--- src/components/icon/chevron/index.tsx
+++ src/components/icon/chevron/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 interface ChevronIconProps {
-  isOpen: boolean;
+  isOpen?: boolean;
 }
 
 const ChevronIcon: FC<ChevronIconProps> = ({ isOpen }) => (
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,2 +1,6 @@
+export { default as AddIcon } from "./add-icon";
+export { default as ChevronIcon } from "./chevron";
+export { default as CloseIcon } from "./close-icon";
 export { default as HamburgerIcon } from "./hamburguer";
+export { default as RemoveIcon } from "./remove-icon";
 export { default as SearchIcon } from "./search";

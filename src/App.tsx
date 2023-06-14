5b3529623ec87a6ca4c86758aae482f1f56383e7
Mateus Jesus <mateusgianetti1996@gmail.com>
2023-06-14 19:30:46 -0300

    feat(basket-context): integrated components to basket II

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BasketProvider } from "@contexts/basket";
 import OnlineMenu from "./pages/online-menu/index";
 
 function App() {
-  return <OnlineMenu />;
+  return (
+    <BasketProvider>
+      <OnlineMenu />
+    </BasketProvider>
+  );
 }
 
 export default App;
